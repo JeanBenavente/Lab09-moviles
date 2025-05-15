932a29c40fd739db8ee5ef007cb2ba6c0514afe3
JeanBenavente <jean.benavente@tecsup.edu.pe>
2025-05-15 13:53:08 -0500

    actualizacion dependencias | corregido

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
 
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    S
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
